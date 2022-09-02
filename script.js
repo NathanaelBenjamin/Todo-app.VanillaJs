32325d68ab63fb0764e1b9a60dfc197012b012d3
Nathanael Benjamin <oluwakayodenathanael@gmail.com>
2022-09-02 19:28:30 +0100

    CSS styles added- Tailwind CSS

--- script.js
+++ script.js
@@ -22,7 +22,10 @@ function showTasks(nameOfTask){
 
     //ADDING CONTENT TO THE ELEMENTS
 
-    deleteButton.innerText = "Delete task"
+    deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+  </svg>
+  `;
 
     checkBox.setAttribute("type", "checkbox");
 
@@ -41,11 +44,12 @@ function showTasks(nameOfTask){
     //APPENDING ELEMENTS TO THEIR PARENTS.
     checkBoxDiv.appendChild(checkBox);
     taskDiv.appendChild(taskName);
+    taskDiv.appendChild(checkBoxDiv);
 
     taskName.innerText = nameOfTask;
 
     todoDiv.appendChild(taskDiv);
-    todoDiv.appendChild(checkBoxDiv);
+    //todoDiv.appendChild(checkBoxDiv);
     todoDiv.appendChild(deleteButton);
 
     listItem.appendChild(todoDiv);
@@ -53,6 +57,8 @@ function showTasks(nameOfTask){
     //ADDING OF CSS CLASSES.
     todoDiv.classList.add("todo");
     taskDiv.classList.add("task");
+    taskName.classList.add("task-name");
+    deleteButton.classList.add("delete");
     
     if(todos.length > 0){
         tasks.appendChild(listItem);
@@ -72,3 +78,19 @@ form.addEventListener("submit", (event) => {
 todos.map(todo => {
     return showTasks(todo);
 });
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+  
+  // Whenever the user explicitly chooses light mode
+  localStorage.theme = 'light';
+  
+  // Whenever the user explicitly chooses dark mode
+  localStorage.theme = 'dark';
+  
+  // Whenever the user explicitly chooses to respect the OS preference
+  localStorage.removeItem('theme');
